4a9078ac4e444dd1aa0a2b199156e2a7680d1017
Emmanuel <ecobian12310@gmail.com>
2026-02-26 13:23:30 -0800

    added suppabase

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,35 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from 'react';
+import './App.css';
+import supabase from './utils/supabase';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [todos, setTodos] = useState([]);
+
+  useEffect(() => {
+    const getTodos = async () => {
+      const { data: todos, error } = await supabase.from('todos').select('*');
+      if (error) console.error(error);
+
+      console.log(todos);
+
+      if (todos.length > 0) {
+        setTodos(todos);
+      }
+    };
+
+    getTodos();
+  }, []);
 
   return (
     <>
+      <h1>Deep Cut</h1>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        {todos.map((todo) => (
+          <li key={todo.id}>{todo.todo}</li>
+        ))}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;

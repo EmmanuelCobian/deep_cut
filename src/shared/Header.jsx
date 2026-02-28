25dd8c9f72f731d01fad69b7cf8e31c5c3204e31
Emmanuel <ecobian12310@gmail.com>
2026-02-28 00:41:53 -0800

    added nav button

--- src/shared/Header.jsx
+++ src/shared/Header.jsx
@@ -1,15 +1,21 @@
 import SearchBar from './SearchBar';
-import { useNavigate } from 'react-router';
+import { useNavigate, useLocation } from 'react-router';
 
 function Header() {
   const navigate = useNavigate();
+  const location = useLocation();
+
+  const isHome = location.pathname === '/';
 
   const handleSearch = (query) => {
     navigate(`/search/${query}`);
-  }
+  };
 
   return (
     <div>
+      <button onClick={() => navigate(-1)} disabled={isHome}>
+        &lt;
+      </button>
       <SearchBar onSearch={handleSearch} />
       <h1>Deep Cut</h1>
     </div>

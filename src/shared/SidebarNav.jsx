1087bf17f4b1e3c9f6eba13ec080f0add22e4c85
Emmanuel <ecobian12310@gmail.com>
2026-02-28 21:44:51 -0800

    updated sidebar

--- src/shared/SidebarNav.jsx
+++ src/shared/SidebarNav.jsx
@@ -1,12 +1,21 @@
 import { Link } from 'react-router';
+import { useAuth } from '../lib/context/AuthContext';
 
 function SidebarNav() {
+  const { user, signOut } = useAuth();
   return (
     <aside>
-      <Link to={'/'}>Home</Link>
-      <Link to={'/journal'}>Journal</Link>
-      <Link to={'/lists'}>Listen List</Link>
-      <Link to={'/account'}>Account</Link>
+      <Link to="/">Home</Link>
+      <Link to="/journal">Journal</Link>
+      <Link to="/lists">Listen List</Link>
+      {user ? (
+        <div>
+          <Link to="/account">Account</Link>
+          <button onClick={signOut}>Log out</button>
+        </div>
+      ) : (
+        <Link to="/login">Log in</Link>
+      )}
     </aside>
   );
 }

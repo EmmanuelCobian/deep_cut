266e8c113b0d0eab4f315e40a0ec2b28e19502d9
Emmanuel <ecobian12310@gmail.com>
2026-03-02 17:02:51 -0800

    added some styling to nav. still in progress

--- src/shared/SidebarNav.jsx
+++ src/shared/SidebarNav.jsx
@@ -4,16 +4,28 @@ import styles from './SidebarNav.module.css';
 
 function SidebarNav() {
   const { user, signOut } = useAuth();
+
   return (
-    <aside>
-      <Link to="/">Home</Link>
-      <Link to="/journal">Journal</Link>
-      <Link to="/lists">Listen List</Link>
-      {user ? (
-        <button onClick={signOut}>Log out</button>
-      ) : (
-        <Link to="/login">Log in</Link>
-      )}
+    <aside className={styles.sidebar}>
+      <div className={styles.topSection}>
+        <div className={styles.logo}>
+          Deep Cut
+        </div>
+
+        <nav className={styles.navLinks}>
+          <Link to="/">Home</Link>
+          <Link to="/journal">Journal</Link>
+          <Link to="/lists">Listen List</Link>
+        </nav>
+      </div>
+
+      <div className={styles.bottomSection}>
+        {user ? (
+          <button onClick={signOut}>Log out</button>
+        ) : (
+          <Link to="/login">Log in</Link>
+        )}
+      </div>
     </aside>
   );
 }

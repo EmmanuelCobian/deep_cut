1aee4b0376601eb667caf6e67359b86f9916b8d7
Emmanuel <ecobian12310@gmail.com>
2026-03-09 16:52:45 -0700

    added styling

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,13 +1,17 @@
 import { useNavigate } from 'react-router';
+import styles from './NotFound.module.css';
 
 function NotFound() {
   const navigate = useNavigate();
 
   return (
-    <div>
-      <h1>404</h1>
-      <p>Page not found</p>
-      <button onClick={() => navigate('/', { replace: true })}>
+    <div className={styles.page}>
+      <h1 className={styles.header}>404</h1>
+      <p className={styles.text}>Page not found</p>
+      <button
+        className={styles.btn}
+        onClick={() => navigate('/', { replace: true })}
+      >
         Go back home
       </button>
     </div>

d0f8acb314480fea36f646570f1825fc541c3c5a
Emmanuel <ecobian12310@gmail.com>
2026-03-09 18:13:14 -0700

    fixed import path

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,7 +1,7 @@
 import { useAuth } from '../lib/context/AuthContext';
 import Loading from '../shared/Loading';
 import Hero from '../features/Hero';
-import Dashboard from '../features//Dashboard/Dashboard';
+import Dashboard from '../features/Dashboard/Dashboard';
 import styles from './Landing.module.css';
 
 function Landing() {
